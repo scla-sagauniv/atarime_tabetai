3f0baad01c734d1c05ccd9f1ed42fad1e211bd06
smaru1111 <marusou20@gmail.com>
2023-04-20 14:09:57 +0900

    #1 add Headerファイル作成

---
+++ img-diary/pages/components/Header.js
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <>
+      <div className="container py-2 px-4 flex justify-between">
+          <div>
+            <a className=" text-4xl">絵日記</a>
+          </div>
+          <div className="">
+            <a>aaaaa</a>
+          </div>
+      </div>
+    </>
+  )
+}
--- img-diary/pages/index.js
+++ img-diary/pages/index.js
@@ -1,9 +1,12 @@
-import { Inter } from 'next/font/google'
+import Header from './components/Header';
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <h1>aaa</h1>
+    <>
+      <main>
+        <Header />
+      </main>
+    </>
   )
 }
