4590719990b2254cda8e59a32a0d08da0553f683
nnnnn81 <nnnnnagi81@gmail.com>
2023-04-22 01:10:14 +0900

    memo: API完成したらendpoint書き換えてね

--- img-diary/lib/FetchChatgpt.js
+++ img-diary/lib/FetchChatgpt.js
@@ -8,7 +8,7 @@ export const FetchChatgpt = async (prompt) => {
             prompt
         })
 }
-  const endpoint = "https://atrm-functions.azurewebsites.net/api/HttpTrigger2?"
+  const endpoint = "https://atrm-functions.azurewebsites.net/api/HttpTrigger2?" // なぎがでぷろいしたやつ 変更可
   const res = await fetch(endpoint,param)
   const data = await res.json()
   return data
