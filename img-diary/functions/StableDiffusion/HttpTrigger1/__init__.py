e9ee534a0976599f2c5cf44472eb42d153900c66
nnnnn81 <nnnnnagi81@gmail.com>
2023-04-22 00:24:10 +0900

    #3 add text-to-image 実装

--- img-diary/functions/StableDiffusion/HttpTrigger1/__init__.py
+++ img-diary/functions/StableDiffusion/HttpTrigger1/__init__.py
@@ -2,23 +2,56 @@ import logging
 
 import azure.functions as func
 
+import io
+import os
+import json
+import base64
+# from PIL import Image
+from dotenv import load_dotenv
+from stability_sdk import client
+import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+
+# APIインタフェースの準備
+load_dotenv()
+stability_api = client.StabilityInference(
+    key=os.environ['STABILITY_KEY'],
+    verbose=True,
+)
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    req_body_bytes = req.get_body()  # byte型で返ってくる
+    req_body = req_body_bytes.decode("utf-8")  # デコードする
+    data = json.loads(req_body)  # jsonとして読み込む
+    prompt = data["prompt"]  # promptを取り出す
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    answers = stability_api.generate(
+        prompt=prompt,
+    )
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-            status_code=200
-        )
+    for resp in answers:
+        for artifact in resp.artifacts:
+            if artifact.finish_reason == generation.FILTER:
+                logging.warn(
+                    "Your request activated the API's safety filters and could not be processed."
+                    "Please modify the prompt and try again.")
+            if artifact.type == generation.ARTIFACT_IMAGE:
+                img = io.BytesIO(artifact.binary)
+                img_bytes = base64.b64encode(
+                    img.getvalue())  # bytesIO型からbytes型にエンコード
+                img_base64 = img_bytes.decode()  # bytes型からstr型にデコード
+                obj = {
+                    "img_base64": img_base64
+                }
+                data = json.dumps(obj)
+                return func.HttpResponse(data)
+
+
+# for resp in answers:
+#     for artifact in resp.artifacts:
+#         if artifact.finish_reason == generation.FILTER:
+#             print("NSFW")
+#         if artifact.type == generation.ARTIFACT_IMAGE:
+#             img = Image.open(io.BytesIO(artifact.binary))
+#             img.save('output.png')
