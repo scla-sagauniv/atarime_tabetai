f4a116473e47068ece8dc854a99d36fb5681bbe7
nnnnn81 <nnnnnagi81@gmail.com>
2023-04-21 16:20:53 +0900

    #7 add ChatgptのAPIを叩く関数の作成

---
+++ img-diary/lib/FetchChatgpt.js
@@ -0,0 +1,15 @@
+export const FetchChatgpt = async (prompt) => {
+  const param = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+            prompt
+        })
+}
+  const endpoint = "/httptrigger2"
+  const res = await fetch(endpoint,param)
+  const data = await res.json()
+  return data
+}
