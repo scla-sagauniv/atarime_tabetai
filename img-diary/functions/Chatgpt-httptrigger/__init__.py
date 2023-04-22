bc0629be4f8f0ab650c4023c1dd5695b9eb827b0
nnnnn81 <nnnnnagi81@gmail.com>
2023-04-23 00:53:25 +0900

    #22 update chatgptAPIからqueueにpromptを渡す

--- img-diary/functions/Chatgpt-httptrigger/__init__.py
+++ img-diary/functions/Chatgpt-httptrigger/__init__.py
@@ -4,23 +4,24 @@ import azure.functions as func
 import json
 import os
 import openai
+import typing
 
 openai.api_key = os.environ['CHATGPT_KEY']
 
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
+def main(req: func.HttpRequest, texts: func.Out[typing.List[str]]) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     req_body_bytes = req.get_body()  # byte型で返ってくる
     req_body = req_body_bytes.decode("utf-8")  # デコードする
     data = json.loads(req_body)  # jsonとして読み込む
     prompt = data["prompt"]  # promptを取り出す
-
+    # logging.info(prompt)
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "100字以内にまとめて"},
             {"role": "system", "content": "日記風で書いて"},  # 制約はなんとか考える
-            {"role": "user", "content": prompt},
+            {"role": "user", "content":  prompt},
         ],
     )
 
@@ -30,12 +31,16 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         "reply": resp_text
     }
     data = json.dumps(obj)
+    prompts = prompt + "," + resp_text
     # return func.HttpResponse(data)
 
     if resp_text:
-        return func.HttpResponse(data)
+        texts.set(prompts)
+        return 'OK'
     else:
         return func.HttpResponse(
             "No ChatGPT response.",
             status_code=200
         )
+    # texts.set("white cat,resp_text")
+    # return func.HttpResponse(prompt)
