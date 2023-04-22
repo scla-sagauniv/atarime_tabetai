559079b501884eed4e5b0d42dd47835fd881a29e
nnnnn81 <nnnnnagi81@gmail.com>
2023-04-23 03:15:53 +0900

    #22 update 画像をBlobにアップロード

--- img-diary/functions/StableDiffusion-queuetrigger/__init__.py
+++ img-diary/functions/StableDiffusion-queuetrigger/__init__.py
@@ -4,11 +4,15 @@ import json
 import logging
 import os
 import typing
+import uuid
 
 import azure.functions as func
+from PIL import Image
 from dotenv import load_dotenv
 from stability_sdk import client
+from azure.identity import DefaultAzureCredential
 import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
 
 load_dotenv()
@@ -21,6 +25,7 @@ stability_api = client.StabilityInference(
 def main(msg: func.QueueMessage) -> None:
     # logging.info('Python queue trigger function processed a queue item: %s',
     #              msg.get_body().decode('utf-8'))
+
     message = msg.get_body().decode('utf-8')
     prompt_list = message.split(',')
     org_prompt = prompt_list[0]
@@ -37,8 +42,43 @@ def main(msg: func.QueueMessage) -> None:
                     "Your request activated the API's safety filters and could not be processed."
                     "Please modify the prompt and try again.")
             if artifact.type == generation.ARTIFACT_IMAGE:
-                img = io.BytesIO(artifact.binary)
+                img = Image.open(io.BytesIO(artifact.binary))
                 logging.info(org_prompt)
+
+                try:
+
+                    print("Azure Blob Storage Python quickstart sample")
+                    account_url = "https://zinbeea369.blob.core.windows.net"
+                    default_credential = DefaultAzureCredential()
+
+                    # Create the BlobServiceClient object
+                    blob_service_client = BlobServiceClient(
+                        account_url, credential=default_credential)
+                    # Create a local directory to hold blob data
+                    local_path = "./data"
+
+                    # Create a file in the local data directory to upload and download
+                    local_file_name = str(uuid.uuid4()) + ".png"
+                    upload_file_path = os.path.join(
+                        local_path, local_file_name)
+
+                    img.save(upload_file_path)
+
+                    # Create a blob client using the local file name as the name for the blob
+                    blob_client = blob_service_client.get_blob_client(
+                        container="diary-images", blob=local_file_name)
+
+                    print("\nUploading to Azure Storage as blob:\n\t" +
+                          local_file_name)
+
+                    # Upload the created file
+                    with open(file=upload_file_path, mode="rb") as data:
+                        blob_client.upload_blob(data)
+                    os.remove(upload_file_path)
+
+                except Exception as ex:
+                    print('Exception:')
+                    print(ex)
                 # img_bytes = base64.b64encode(
                 #     img.getvalue())  # bytesIO型からbytes型にエンコード
                 # img_base64 = img_bytes.decode()  # bytes型からstr型にデコード
