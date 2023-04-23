707a70168bce6eeab439cd1378fef9b0cf6a5d61
nnnnn81 <nnnnnagi81@gmail.com>
2023-04-23 14:52:02 +0900

    update: ローディング画面追加

--- img-diary/pages/result.js
+++ img-diary/pages/result.js
@@ -10,6 +10,7 @@ import { useRouter } from 'next/router';
 export default function  Home (props){
   const [imgSrc, setImgSrc] = useState('https://zinbeea369.blob.core.windows.net/diary-images/output.png')
   const [articleTxt, setArticleTxt] = useState('テキストテキストテキストテキストテキストテキストテキストテキスト')
+  const [isOk, setIsOk] = useState(false)
   const router = useRouter();
 
 
@@ -22,10 +23,14 @@ export default function  Home (props){
     .then((data) => {
       if(data.reply){
         setArticleTxt(data.reply)
+        setIsOk(true)
       }
     }
     ).catch((err) => console.log("error",err))
   })
+  
+  // setTimeout(setImgSrc("https://zinbeea369.blob.core.windows.net/diary-images/output.png"),10000
+  // )
 
   return(
     <>
@@ -34,9 +39,12 @@ export default function  Home (props){
           <Header /> 
           <div>
             <div className="text-center mt-20 text-xl font-bold md:text-3xl">！！完成！！</div>
-              <Diary src={imgSrc} >
+            
+              <div className="flex justify-center m-10">
+                {isOk?  <Diary src={imgSrc} >
                 {articleTxt}
-              </Diary>
+              </Diary>: <progress className="progress w-56"></progress>}
+              </div>
               <div className="flex justify-center m-10">
                 <Button onClick={onClick_d}>
                   ダウンロード
